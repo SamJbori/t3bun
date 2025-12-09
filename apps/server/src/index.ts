89c0936c38b1d1ac9bbd9fed9cc668a9357d2d00
SamJbori <17522597+SamJbori@users.noreply.github.com>
2025-12-09 14:49:00 +0300

    test2

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,9 @@
 import { Hono } from "hono";
-import { x } from "@repo/constants";
 const app = new Hono();
 
 const welcomeStrings = [
   "Hello Hono!",
   "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
-  x,
 ];
 
 app.get("/", (c) => {
