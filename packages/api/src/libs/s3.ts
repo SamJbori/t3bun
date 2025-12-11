f81acd21059a0677c7ac0c85a6ce9c85c642c8b0
SamJbori <17522597+SamJbori@users.noreply.github.com>
2025-12-11 13:32:39 +0300

    add storage

--- packages/api/src/libs/s3.ts
+++ packages/api/src/libs/s3.ts
@@ -2,6 +2,6 @@ import type { S3Options } from "bun";
 import { s3 } from "bun";
 
 export const storage = {
-  delete: (file: string, options: S3Options) => s3.delete(file, options),
+  delete: (file: string, options?: S3Options) => s3.delete(file, options),
   // add other operations as needed
 };
