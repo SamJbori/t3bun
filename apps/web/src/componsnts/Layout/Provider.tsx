6eeda9800d6f71a214c136f4912994fb25712988
SamJbori <17522597+SamJbori@users.noreply.github.com>
2025-12-13 14:19:15 +0300

    update:gh

--- apps/web/src/componsnts/Layout/Provider.tsx
+++ apps/web/src/componsnts/Layout/Provider.tsx
@@ -2,7 +2,7 @@
 
 import type { ReactNode } from "react";
 import { useMemo } from "react";
-import useAnonymousLogin from "@hooks/useAnonymousLogin";
+// import useAnonymousLogin from "@hooks/useAnonymousLogin";
 import { env } from "@libs/env";
 import { getQueryClient } from "@libs/tquery";
 import { api, getTRPCClient } from "@libs/trpc";
@@ -10,11 +10,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  const isSession = useAnonymousLogin();
+  // const isSession = useAnonymousLogin();
 
   const queryClient = useMemo(() => getQueryClient(), []);
 
-  if (!isSession) return <div />;
+  // if (!isSession) return <div />;
 
   return (
     <QueryClientProvider client={queryClient}>
